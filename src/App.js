3d8b6559e917e8255feff372b3f21fc935c64638
GoodyCalled <melissa@magnetic.services>
2025-08-25 01:15:59 -0500

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AIUsageScorecard from './AIUsageScorecard';
+
+function App() {
+  return (
+    <div className="App">
+      <AIUsageScorecard />
+    </div>
+  );
+}
+
+export default App;
